60ea3cff379f199e4cad987a16ada0882a78b02b
Tristan B. Kildaire <deavmi@redxen.eu>
2021-11-15 18:26:44 +0200

    Re-ordered type definitions
    Added new Coordinate type

--- types.h
+++ types.h
@@ -2,6 +2,38 @@
 #include<stdint.h>
 
 /**
+ * @brief Coordinate
+ * 
+ * Just plain old (x,y) coordinates
+ */
+struct Coordinate
+{
+    uint32_t x;
+    uint32_t y;
+};
+typedef struct Coordinate Coordinate;
+
+
+/**
+ * @brief TedScreen
+ * 
+ * This is what one sees finally on his screen
+ * when using the editor. This houses position
+ * of the TTY cursor relative to the sub-section
+ * of the TTY that is used for the editor window
+ */
+struct TedScreen
+{
+    /* (x,y) position */
+    Coordinate cursor;
+
+    /* Screen offset from top and right hand side */
+    Coordinate min;
+    Coordinate max;
+};
+typedef struct TedScreen TedScreen;
+
+/**
  * @brief TedSession
  * 
  * Reppresents an editor session which is
@@ -17,31 +49,12 @@ struct TedSession
     /* File being edited */
     uint32_t fd;
 
+    /* Screen */
+    TedScreen* screen;
+
     /* Next session */
 };
 typedef struct TedSession TedSession;
-
-/**
- * @brief TedScreen
- * 
- * This is what one sees finally on his screen
- * when using the editor. This houses position
- * of the TTY cursor relative to the sub-section
- * of the TTY that is used for the editor window
- */
-struct TedScreen
-{
-    /* (x,y) position */
-    uint32_t x;
-    uint32_t y;
-
-    /* Screen offset from top and right hand side */
-    uint32_t minX;
-    uint32_t minY;
-    uint32_t maxX;
-    uint32_t maxY;
-};
-typedef struct TedScreen TedScreen;
 
 /**
  * @brief TedState
@@ -57,9 +70,6 @@ struct TedState
 
     /* TTY device */
     uint32_t ttyFD;
-
-    /* Screen */
-    TedScreen* screen;
 
     /* Open Sessions */
     TedSession* sessions;
